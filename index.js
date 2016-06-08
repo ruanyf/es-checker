0a510099d2c0b85c3899d9b68f577f07f8780f39
ruanyf <yifeng.ruan@gmail.com>
2016-06-08 09:48:44 +0800

    fix: change Object.getOwnSymbolNames to Object.getOwnPropertySymbols

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var supports = require('./lib/interface');
 var api = require('./lib/api');
 var runTest = require('./lib/runtest');
 
-for (var key in supports){
+for (var key in supports) {
   supports[key] = runTest(getApiName(key));
 }
 
 supports._api = api;
 
-function getApiName(key){
+function getApiName(key) {
   return supports[key];
 }
 
